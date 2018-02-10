765cd76e47c8f7cebcbb6267594f1587a3e4fd8d
Mathias Cousté <couste.mathias@gmail.com>
2018-02-10 15:04:48 +0000

    embedded working now

--- smart-thermostat-embedded/run.py
+++ smart-thermostat-embedded/run.py
@@ -7,11 +7,23 @@ import time
 import sys
 import requests
 import json
+import RPi.GPIO as GPIO
 
-web_loop_duration = 10
-main_loop_duration = 1
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+
+
+web_loop_duration = 300
+main_loop_duration = 30
 
 days = {"0" : "SUN", "1" : "MON", "2" : "TUE", "3" : "WED", "4" : "THU", "5" : "FRI", "6" : "SAT"}
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+firebase_url = 'https://smart-thermostat-2c65a.firebaseio.com/temperature.json'
 
 temp_buffer = []
 temp_buffer_index = 0
@@ -23,10 +35,22 @@ targets = None
 expected_temperature = 0
 
 print(" > Defining methods...")
-# Mocked temperature reading
+def read_temp_raw():
+  f = open(device_file, 'r')
+  lines = f.readlines()
+  f.close()
+  return lines
+
 def read_temp():
   global current_temperature
-  current_temperature = 19.8
+  lines = read_temp_raw()
+  while lines[0].strip()[-3:] != 'YES' :
+    time.sleep(0.2)
+    lines = read_temp_raw()
+  equals_pos = lines[1].find('t=')
+  if equals_pos != -1:
+    temp_string = lines[1][equals_pos+2:]
+  current_temperature = float(temp_string) / 1000.0
 
 def computeExpectedTemperature():
   global expected_temperature
@@ -49,10 +73,10 @@ def computeExpectedTemperature():
     expected_temperature = yA + coef * (current_time_in_minutes - xA)
 
 def on():
-  print(" -- ON -- ")
+  GPIO.output(18,GPIO.HIGH)
 
 def off():
-  print(" -- OFF -- ")
+  GPIO.output(18,GPIO.LOW)
 
 def checkTemperature():
   computeExpectedTemperature()
@@ -68,7 +92,7 @@ def calculateAverageTemperature():
     temp_buffer.append(current_temperature)
   else:
     temp_buffer[temp_buffer_index] = current_temperature
-  
+
   temp_buffer_index = temp_buffer_index + 1
   if(temp_buffer_index >= buffer_len):
     temp_buffer_index = 0
@@ -78,13 +102,14 @@ def calculateAverageTemperature():
 def sendToFirebase():
   date = time.strftime('%Y%m%d%H%M%S')
   data = {'date':date,'value':current_average_temperature}
-  print(json.dumps(data))
+  result = requests.post(firebase_url, data=json.dumps(data))
 
 def downloadTargets():
   global targets
   day = days.get(time.strftime('%w'))
   r = requests.get("https://smart-thermostat-2c65a.firebaseio.com/settings/-L3x0J_gL97IVtVl-zzg/" + day +".json")
-  targets = r.json()
+  if(r.status_code == 200):
+    targets = r.json()
 
 def executeWebLoop():
   downloadTargets()
@@ -102,4 +127,3 @@ try:
     time.sleep(main_loop_duration)
 except KeyboardInterrupt:
   sys.exit(0)
-
